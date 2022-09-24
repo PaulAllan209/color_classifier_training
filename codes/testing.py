850fd9dc7ad3c1dc34c9eecbff8d28014e790e7c
PaulAllan209 <dpaulallan15@gmail.com>
2022-09-24 15:43:20 +0800

    changed the code and added new directories

--- codes/testing.py
+++ codes/testing.py
@@ -1,10 +1,24 @@
-import numpy as np
+from datetime import datetime
+import os
 
-ar = np.array([
-    [1, 2, 3],
-    [3, 2 ,5],
-    [3, 4, 5]
-])
+# global models_path
+# models_path = "./trained_models"
 
-rows, columns = np.where((ar == (1, 2, 3)).all(axis=1))
-print(rows)
+
+# if os.listdir(f"{models_path}"):
+#     last_model_num = os.listdir(f"{models_path}")[-1][-1]
+#     os.mkdir(f"{models_path}/model{int(last_model_num) + 1}")
+    
+#     # Makes a readme file that contains information on when was this created
+#     last_model = os.listdir(f"{models_path}")[-1]
+#     with open(f"{models_path}/{last_model}/readme.txt", "w+") as f:
+#         f.write(f"Model created on: {str(datetime.now())}")
+
+# elif not os.listdir(f"{models_path}"):
+#     os.mkdir(f"{models_path}/model1")
+
+#     # Makes a readme file that contains information on when was this created
+#     with open(f"{models_path}/model1/readme.txt", "w+") as f:
+#         f.write(f"Model created on: {str(datetime.now())}")
+
+print(datetime.now())
