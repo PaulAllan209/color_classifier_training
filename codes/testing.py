f6dc1a65bb9833372280ed1f6ebbc3dcb58c7fb5
PaulAllan209 <dpaulallan15@gmail.com>
2022-09-27 10:24:28 +0800

    A lot of changes and added new example
    Deleted the models, placed the model maker class in a separate python file, renamed the class to make it more specific, added new example number_classifier

--- codes/testing.py
+++
@@ -1,7 +0,0 @@
-anon_func = lambda x: min(x, key=lambda i:i[-1])
-
-test_list = ['wow1', 'wow2', 'wow3', 'wow4']
-test_list1 = ['wo1', 'wo2', 'wo3', 'wo4']
-
-print(anon_func(test_list))
-print(anon_func(test_list1))
